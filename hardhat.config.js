0fe32c0bba69414766e36bafe28c8274ae77696e
TokenRainbowSix <134370409+TokenRSIX@users.noreply.github.com>
2023-08-27 14:53:25 +0200

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.16",
+  networks: {
+    rinkeby: {
+       
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
